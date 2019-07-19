df7e359aecb92ae7ba1ec69e1ece03c8e9fd7ff3
Harum Helmy <harumendhah@gmail.com>
2019-07-19 14:47:25 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.destroy_all
+
+Student.create(first_name: "Kacey", last_name: "Musgraves")
+Student.create(first_name: "adrienne", last_name: "brown")
+Student.create(first_name: "Harum", last_name: "Helmy")
+Student.create(first_name: "Austen", last_name: "Mack-Crane")
+Student.create(first_name: "Rihanna", last_name: "Fenty")
